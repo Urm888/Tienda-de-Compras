153971baaa005226c805e7d9b71137ac70aaf3e6
Uriel Red <Urmecdj_8@outlook.com>
2022-01-02 16:57:35 -0600

    Modificando codigo en Javascript

---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+let search=document.querySelector(search);
+let iconsearch=document.querySelector(iconsearch);
+let iconshop=document.querySelector(Iconshop);
+
+
+//Funcion para crear busqueda
+function buscar(){
+    filter=document
+}
