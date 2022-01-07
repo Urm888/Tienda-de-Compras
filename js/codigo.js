4de3c37963d34a8d90b0f36aa808b07c21ef122e
Uriel Red <Urmecdj_8@outlook.com>
2022-01-06 18:30:02 -0600

    no se puede jajaja

--- js/codigo.js
+++ js/codigo.js
@@ -1,19 +1,37 @@
-let search_=document.getElementById("search");
-let i_consearch=document.getElementById("iconsearch");
-let i_conshop=document.getElementById("iconshop");
-let box_search=document.getElementById("boxsearch")
+const expresiones = {
+	user: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+
+function iniciar(){
+   var    boton=document.getElemetById("enviar");
+          boton.addEventListener("click",formulario); 
+          
+} 
+function formulario(){
+   nombre=document.getElementById("Nombre").tolowUpperCase();
+   if(expresiones.name=nombre){
+      alert(nombre);
+   } else{alerrt("solo numeros")
+
+   }
 
 
-//ejecutar funcion para mostrar caja de busqueda
-document.getElementById("search").addEventListener("keyup",buscador);
-
-//Funcion para el buscador
-var lista=["Lavadora","Estufa","Refrigerador","Microondas"]
-for (i=0; i<lista.lenght; i++){
-    document.write(lista[0]);
-
+   
 }
+formulario();
 
 
-}
+
+
+
+ 
+
+
+ 
+
+
+
 
