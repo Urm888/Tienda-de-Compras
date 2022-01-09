dfb9dda785f3897cf106208bcc8b57b0481f431a
Uriel Red <Urmecdj_8@outlook.com>
2022-01-09 16:17:47 -0600

    codigo js validacion formulario de login

--- js/codigo.js
+++ js/codigo.js
@@ -1,36 +1,52 @@
+window.onload=iniciar;
 const expresiones = {
 	user: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-
-function iniciar(){
-   var    boton=document.getElemetById("enviar");
-          boton.addEventListener("click",formulario); 
-          
-} 
-function formulario(){
-   nombre=document.getElementById("Nombre").tolowUpperCase();
-   if(expresiones.name=nombre){
-      alert(nombre);
-   } else{alerrt("solo numeros")
-
-   }
+	name: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{6,15}$/, // 6 a 15 digitos.
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	cellphone: /^\d{7,14}$/ // 7 a 14 numeros.
+}  
+ const divInput=document.querySelectorAll(".input-contenedor");
+ const form=document.getElementById("formulario")
 
 
-   
+ function iniciar(){
+   booton=document.getElementById("enviar");
+   booton.addEventListener("click",ejecutarBoton);}  
+
+ function ejecutarBoton(e){    
+   e.preventDefault();
+   divInput.forEach((element)=>{
+   element.lastElementChild.innerHTML="";});
+   validacionForm()
+   mensajeDeEnvio()
 }
-formulario();
+
+  function validacionForm (){
+   let=condicion=true;
+   nombre=document.getElementById("nombre").value;
+   if(!expresiones.name.test(nombre)){ 
+    mensajeDeError("nombre","ERROR: Ingrese Su Nombre");}
+
+   correo=document.getElementById("correo").value;
+   if(!expresiones.email.test(correo)){
+    mensajeDeError("correo","ERROR: Ingrese un correo Valido");}
+
+   contraseña=document.getElementById("contraseña").value;
+   if(!expresiones.password.test(contraseña)){
+    mensajeDeError("contraseña","ERROR: El minimo de Caracteres validos son 6");} 
+}
+
+function mensajeDeError(input,mensaje){
+   let elemento=document.querySelector(`.${input}`)
+       elemento.lastElementChild.innerHTML=mensaje;
+ }
+
+ function mensajeDeEnvio(){
+   form.reset();
+}
 
 
-
-
-
- 
-
-
- 
 
 
 
