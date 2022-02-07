68a92b501a671a2f56fb915b28213f436a6e1e04
Uriel Red <Urmecdj_8@outlook.com>
2022-02-07 14:18:07 -0600

    aparece problema css

--- js/search.js
+++ js/search.js
@@ -3,46 +3,15 @@ const buscador=document.querySelector("#search");
 const caja=document.querySelector("#boxsearch");
 
 
-
-const productos=[
-    {nombre:"Adidas"}, {nombre: "Almohada"}, {nombre: "Audifonos"},
-    {nombre: "Cafetera"},{nombre: "Cnc"},{nombre: "Controles"},
-    {nombre: "DC"},
-    {nombre: "Esmeril"},{nombre: "Escritorio"},
-    {nombre: "Hisense"},
-    {nombre: "Jordan"},
-    {nombre: "Lavadoras"}, {nombre: "Lapiceras"}, {nombre: "Laptop"},    {nombre: "LG"},{nombre: "Licuadora"},
-    {nombre: "Martillo"}, {nombre: "Microondas"},
-    {nombre: "Nike"},
-    {nombre: "Samsung"},
-    {nombre: "Tostadora"},
-    {nombre: "Vans"},{nombre: "Vino"},
-    {nombre: "Zapatos"},
-]
-
 function iniciar(){
-    filtro=document.querySelector("#iconsearch");   
-    buscador.addEventListener("keyup",menu);
-       }
-
-function menu(){
-    caja.innerHTML=``;
-    busqueda=buscador.value.toLowerCase();
-    for( let producto of productos){ 
-        let lista=producto.nombre.toLowerCase();
-        if(lista.indexOf(busqueda) !==-1){
-            caja.style.display="block";
-            if(search.value ===""){
-                caja.style.display="none"; }            
-                caja.innerHTML +=
-                `<li><a href="Adidas.html">${producto.nombre}<a/></li>`;
-                `<li><a href="Almohadas.html">${producto.nombre}<a/></li>`;
-     }
-    }
+ buscador.value.toLowerCase();
+ lista.addEventListenner("keyup",filtro);
 }
 
+function filtro(){
+ console.log(lista.value);
 
-
+}
 
 
 
