e9febf1ac1aad55eb9abc89082d65cf3b1e7b044
Uriel Red <Urmecdj_8@outlook.com>
2022-08-20 22:54:00 -0500

    Seguimos con el problema en JavaScrip de los enlaces al encontrar un producto

--- js/search.js
+++ js/search.js
@@ -1,19 +1,42 @@
 window.load = iniciar;
+const search=document.querySelector("#search");
+const icon=document.querySelector("#iconsearch");
+const list=document.querySelector("#boxsearch");
+      search.addEventListener("keyup",iniciar);
+           
+
+const articulos = [
+      {nombre: `Adidas`},
+      {nombre: `Almohada`},
+      {nombre: `Audifonos`},
+]
+
 
 function iniciar(){
- const buscador=document.querySelector(`search`);
-       buscador.addEventListener("click",buscar)
+    caja=search.value.toLowerCase();
+    list.innerHTML=``;
+
+  for(let articulo of articulos){
+  let lista=articulo.nombre.toLowerCase();          
+  if(lista.indexOf(caja) !== -1){
+  list.style.display="block";        
+  list.innerHTML+=
+  `<li><a href="Adidas.html">${articulo.nombre} </a><li/>`
+  `<li><a href="Almohadas.html">${articulo.nombre} </a><li/>`
+ 
 }
-
-function buscar(){
-  caja=buscador.value.toLowerCase();
-  console.log(caja.value);
+}          
+              
+  if(search.value ===""){
+  list.style.display="none"
+  
 }
+}
+iniciar();
 
 
 
-
-
+// El siguiente codigo Carrito de Compras
 
 
 
